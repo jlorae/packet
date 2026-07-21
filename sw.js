92797a67a48a4d436db3f2174f174dcc1b8c6967
Jessica Perez <jessica@itspuremotion.com>
2026-07-21 09:52:10 -0500

    packet 2026-07-21

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 // bump CACHE to force clients to pick up a new shell
-const CACHE = "packet-20260721094911";
+const CACHE = "packet-20260721095204";
 const SHELL = ["./", "index.html", "manifest.webmanifest", "icon-192.png", "icon-512.png", "icon-180.png", "landing.jpg"];
 self.addEventListener("install", e => { self.skipWaiting();
   e.waitUntil(caches.open(CACHE).then(c => c.addAll(SHELL))); });
